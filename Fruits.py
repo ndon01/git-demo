34133adadb805e04761007a0351ea42aa8341577
nickdonahue <117882806+nickdonahue@users.noreply.github.com>
2023-08-27 21:47:25 -0400

    Changed pumpin from veggie to fruit

--- Fruits.py
+++ Fruits.py
@@ -1,1 +1,1 @@
-fruits = ['apple', 'dragonfruit', 'peach', 'banana', 'grape', 'apple', 'peach', 'watermelon', 'grape', 'grape']
+fruits = ['apple', 'pumpkin', 'dragonfruit', 'peach', 'banana', 'grape', 'apple', 'peach', 'watermelon', 'grape', 'grape']
